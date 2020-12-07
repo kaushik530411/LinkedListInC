dcad552fe4f5fb8a5f83f19a2a36fc47a11c65f1
Kaushik Mishra <kaushik.mishra@bison.howard.edu>
2020-12-07 18:38:02 +0000

    Added all implementation.

--- list/list.c
+++ list/list.c
@@ -5,23 +5,164 @@
 // <Author>
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "list.h"
 
-list_t *list_alloc() { return NULL; }
-void list_free(list_t *l) {}
+list_t *list_alloc() {
+  list_t* new_list_t = malloc(sizeof(list_t));
+  new_list_t->head = NULL;
+  return new_list_t; 
+}
 
-void list_print(list_t *l) {}
-int list_length(list_t *l) { return -1; }
+void node_free(node_t *n) {
+  if (n->next != NULL){
+    node_free(n->next);
+  } else {
+    free(n);
+  }
+}
+void list_free(list_t *l) {
+  node_t* curr = l->head;
+  if (curr != NULL){
+    node_free(curr);
+  }
+}
 
-void list_add_to_back(list_t *l, elem value) {}
-void list_add_to_front(list_t *l, elem value) {}
-void list_add_at_index(list_t *l, elem value, int index) {}
+void list_print(list_t *l) {
+  node_t* curr = l->head;
+  while(curr != NULL) {
+    printf("%d", curr->value);
+		printf("\n");
+    curr = curr->next;
+  }
+  printf("\n");
+}
 
-elem list_remove_from_back(list_t *l) { return -1; }
-elem list_remove_from_front(list_t *l) { return -1; }
-elem list_remove_at_index(list_t *l, int index) { return -1; }
+int list_length(list_t *l) {
+  int len = 0;
+  node_t* curr = l->head;
+  while(curr != NULL) {
+    len += 1;
+    curr = curr->next;
+  }
+  return len; 
+}
 
-bool list_is_in(list_t *l, elem value) { return false; }
-elem list_get_elem_at(list_t *l, int index) { return -1; }
-int list_get_index_of(list_t *l, elem value) { return -1; }
+void list_add_to_back(list_t *l, elem value) {
+  node_t* curr = l->head;
+  node_t* new = malloc(sizeof(node_t));
+  new->value = value;
+  new->next = NULL;
+  if (curr == NULL){
+    l->head = new;
+    return;
+  }
+  while (curr->next != NULL) {
+    curr = curr->next;
+  }
+  curr->next = new;
+}
+
+void list_add_to_front(list_t *l, elem value) {
+  node_t* new = malloc(sizeof(node_t));
+  new->value = value;
+  new->next = l->head;
+  l->head = new;
+}
+void list_add_at_index(list_t *l, elem value, int index) {
+  if ((index < 0) || (list_length(l) <= index)) {
+    return;
+  }
+  node_t* new = malloc(sizeof(node_t));
+  new->value = value;
+  int curr_index = 0;
+  node_t* curr = l->head;
+  while (curr_index+1 != index) {
+    curr_index += 1;
+    curr = curr->next;
+  }
+  if (curr == NULL) {
+    l->head = new;
+  } else {
+		printf("here is fine \n");
+		new->next = curr->next;
+		curr->next = new;
+  }
+}
+
+elem list_remove_from_back(list_t *l) {
+  node_t* curr = l->head;
+  node_t* result = curr;
+  while((curr != NULL) && (curr->next != NULL)) {
+    if ((curr->next->next == NULL)) {
+      result = curr->next;
+      curr->next = NULL;
+    } else {
+      curr = curr->next;
+    }
+  }
+  return result->value;
+}
+
+elem list_remove_from_front(list_t *l) {
+  if (l->head == NULL) {
+    return 0;
+  } else {
+    int val = l->head->value;
+    l->head = l->head->next;
+    return val;
+  }
+}
+
+elem list_remove_at_index(list_t *l, int index) {
+  if ((index < 0) || (list_length(l) <= index)) {
+    return;
+  }
+  int curr_index = 0;
+  node_t* curr = l->head;
+  while (curr_index+1 != index) {
+    curr_index += 1;
+    curr = curr->next;
+  }
+  int result = curr->next->value;
+  curr->next = curr->next->next;
+  return result;
+}
+
+bool list_is_in(list_t *l, elem value) {
+  node_t* curr = l->head;
+  while (curr != NULL) {
+    if (curr->value == value) {
+      return true;
+    }
+    curr = curr->next;
+  }
+  return false;
+}
+
+elem list_get_elem_at(list_t *l, int index) {
+  if ((index < 0) || (list_length(l) <= index)) {
+    return -1;
+  }
+  int curr_index = 0;
+  node_t* curr = l->head;
+  while (curr_index+1 != index) {
+    curr_index += 1;
+    curr = curr->next;
+  }
+  return curr->next->value;
+}
+
+int list_get_index_of(list_t *l, elem value) {
+  node_t* curr = l->head;
+  int curr_index = 0;
+  while (curr != NULL) {
+    if (curr->value == value) {
+      return curr_index;
+    }
+    curr = curr->next;
+    curr_index += 1;
+  }
+  return -1;
+}
--- list/main.c
+++ list/main.c
@@ -1,8 +1,95 @@
 #include <stdio.h>
+#include <assert.h>
 
 #include "list.h"
+void initialize(list_t* l) {
+  list_add_to_back(l, 2);
+  list_add_to_back(l, 11);
+  list_add_to_back(l, 16);
+  list_add_to_back(l, 17);
+}
 
+void test_basics() {
+  printf("First Test run \n");
+  list_t* list_1 = list_alloc();
+  initialize(list_1);
+  list_print(list_1);
+  assert(list_length(list_1) == 4);
+	list_remove_from_back(list_1);
+	list_print(list_1);
+  assert(list_length(list_1) == 3);
+  list_free(list_1);
+  printf("Basic tests passed. \n");
+}
+
+void test_add_remove_front() {
+	printf("Second Test run \n");
+  list_t* list_1 = list_alloc();
+	list_add_to_front(list_1, 1);
+	initialize(list_1);
+  list_print(list_1);
+  assert(list_length(list_1) == 5);
+	list_remove_from_front(list_1);
+  list_print(list_1);
+  assert(list_length(list_1) == 4);
+  list_free(list_1);
+  printf("Test two passed. \n");
+}
+
+void test_add_remove_specific_index() {
+	printf("Third Test run \n");
+  list_t* list_1 = list_alloc();
+	initialize(list_1);
+	list_add_at_index(list_1, 20, 1);
+  list_print(list_1);
+  assert(list_length(list_1) == 5);
+	
+	list_remove_at_index(list_1, -10);
+  list_remove_at_index(list_1, 10);
+  list_remove_at_index(list_1, 1);
+  list_print(list_1);
+  assert(list_length(list_1) == 4);
+  list_free(list_1);
+  printf("Test three passed. \n");
+}
+
+void test_element_exists() {
+	printf("Fourth Test run \n");
+  list_t* list_1 = list_alloc();
+	initialize(list_1);
+	assert(list_is_in(list_1, 15) == false);
+  assert(list_is_in(list_1, 16) == true);
+	list_free(list_1);
+  printf("Test four passed. \n");
+}
+
+void test_get_element_sepecific_index() {
+	printf("Fifth Test run \n");
+	list_t* list_1 = list_alloc();
+	initialize(list_1);
+	assert(list_get_elem_at(list_1, 1) == 11);
+  assert(list_get_elem_at(list_1, 10) == -1);
+  assert(list_get_elem_at(list_1, -10) == -1);
+	printf("Test five passed. \n");
+}
+
+void test_find_first_occurence() {
+  printf("Sixth Test run \n");
+	list_t* list_1 = list_alloc();
+	initialize(list_1);
+  assert(list_get_index_of(list_1, 11) == 1);
+  assert(list_get_index_of(list_1, 51) == -1);
+  list_free(list_1);
+  printf("Test six passed. \n");
+}
 int main() {
   printf("Tests for linked list implementation\n");
+  test_basics();
+	test_add_remove_front();
+	test_add_remove_specific_index();
+	test_element_exists();
+	test_get_element_sepecific_index();
+  test_find_first_occurence();
+  printf("All tests for linked list implementation PASSED.\n");
   return 0;
 }
